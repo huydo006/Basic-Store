50e9bf1efacd4958be3a3c385d1133f760a1262c
Huy <dohuynm123@gmail.com>
2026-09-11 15:45:26 +0700

    First Commit

---
+++ src/main/java/org/example/store_byspringboot/StoreBySpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.store_byspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBySpringBootApplication.class, args);
+    }
+
+}
